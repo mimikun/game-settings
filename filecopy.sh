9ae62279c2ced4afdaac923e72254b6a1448f52a
Yuto Tanaka <karaage_nennen@icloud.com>
2021-09-19 00:51:36 +0900

    Initial commit

---
+++ filecopy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WIN_HOME="/mnt/c/Users/dzdzb"
+
+cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/local/previousgamestate.txt ./Apex_Legends
+cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/local/settings.cfg ./Apex_Legends
+cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/local/videoconfig.txt ./Apex_Legends
+cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/profile/profile.cfg ./Apex_Legends
