ec8a8d7d366ab9cfc287277993fdfce0ce8139c4
Yuto Tanaka <karaage_nennen@icloud.com>
2021-10-13 21:08:02 +0900

    Update

--- filecopy.sh
+++ filecopy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-WIN_HOME="/mnt/c/Users/dzdzb"
-
 cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/local/previousgamestate.txt ./Apex_Legends
 cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/local/settings.cfg ./Apex_Legends
 cp ${WIN_HOME}/Saved\ Games/Respawn/Apex/local/videoconfig.txt ./Apex_Legends
